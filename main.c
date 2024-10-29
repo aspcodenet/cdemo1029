40fb355fa8887f45a2bdc2256b5b994369df1cb4
Stefan Holmberg <stefan.holmberg@systementor.se>
2024-10-29 10:58:36 +0100

    aaa

--- main.c
+++ main.c
@@ -10,12 +10,36 @@
 // DEKLARERA (lova att det kommer en funktion som heter showMenu)
 void showMenu();
 
+void addera(int c){
+    int t = 123;
+    //char whatever[10] = "blabla";
+    c = c + 12;
+    printf("%d\n", c);
+}
+void adderaAndModifyOriginal(int *c){ // i variablens c ligger en address
+    int t = 123;
+    //char whatever[10] = "blabla";
+    *c = *c + 12;
+    printf("%d\n", c);
+}
 
-int main() {  
+
+/*
+1. 11 23 11
+2. 11 23 23
+3. 11 11 11
+*/ 
+int main() { 
+    int b = 99;
+    int c = 11;
+    char namn[15] = "Stefan";
+    printf("%d\n", c); // 11
+    adderaAndModifyOriginal(&c); // 1004
+    printf("%d\n", c); // 23
+ 
     while(true){
         showMenu();
         int selection = getMenuInput(1,4);
- 
         //int selection = getMenuInput(100,102);
  
         if(selection == 4){
